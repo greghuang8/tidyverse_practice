f0f6c24cbb8214739020b30e2ae403a318caa1fe
Greg Huang <gregoryhuang2005@gmail.com>
2020-12-15 16:35:53 +0800

    Day 3 data and solution

--- acoc.R
+++ acoc.R
@@ -36,5 +36,70 @@ day2 <- read_csv("day2.csv") %>%
          secondLoc = str_sub(password,max,max))%>%
   filter(xor(firstLoc == letter, secondLoc == letter)) #Part 2 solution; 335 obs
 
+# Day 3
+day3 <- read_csv("day3.csv", col_names = FALSE) %>%
+mutate(X1 = str_dup(X1, 100))
+
+index <- 4
+tree3 <- 0
+for (x in 2:323){
+  if(str_sub(day3$X1[x],index,index) == "#"){
+    tree3 <- tree3 + 1
+  }
+  index <- index + 3
+}
+
+# Day 3: Part 2, other parameters
+# Right 1, Down 1
+index <- 2
+tree1 <- 0
+x <- 0
+for (x in 2:323){
+  if(str_sub(day3$X1[x],index,index) == "#"){
+    tree1 <- tree1 + 1
+  }
+  index <- index + 1
+}
+
+#Right 5, Down 1
+index <- 6
+tree5 <- 0
+x <- 0
+for (x in 2:323){
+  if(str_sub(day3$X1[x],index,index) == "#"){
+    tree5 <- tree5 + 1
+  }
+  index <- index + 5
+}
+
+#Right 7, Down 1
+index <- 8
+tree7 <- 0
+x <- 0
+for (x in 2:323){
+  if(str_sub(day3$X1[x],index,index) == "#"){
+    tree7 <- tree7 + 1
+  }
+  index <- index + 7
+}
+
+#Right 1, Down 2
+index <- 2
+tree2 <- 0
+x <- 0
+for (x in seq(3,323,by=2)){
+  if(str_sub(day3$X1[x],index,index) == "#"){
+    tree2 <- tree2 + 1
+  }
+  index <- index + 1
+}
+
+resultDay3 <- tree1*tree2*tree3*tree5*tree7
+
+# Hmm. A %% solution should be faster here, but 
+# str_dup() by 100 gets it done instantly still...
+
+
+# Day 4
 
 
